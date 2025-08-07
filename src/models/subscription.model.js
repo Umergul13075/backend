8c9af652a23d8447eb7aa35f4e885a913e08bbde
Umergul13075 <145903053+Umergul13075@users.noreply.github.com>
2025-08-08 03:33:09 +0500

    Add subscription model

---
+++ src/models/subscription.model.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const subscriptionSchema = mongoose.Schema({
+    subscriber: { // woh jo subscribe kr rha hai
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    channel:{ // jis ko subscribe kr rha hai subscriber
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }
+},{timestamps: true})
+
+export const Subscription = mongoose.model("Subscription", subscriptionSchema)
